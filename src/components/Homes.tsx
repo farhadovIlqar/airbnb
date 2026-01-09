02c016b7724c161e965a00c8666e5abae52a4b4d
ferhadovss <ferhad.ov.ilqar0@gmail.com>
2026-01-09 19:12:09 +0400

    Created Home Page

---
+++ src/components/Homes.tsx
@@ -0,0 +1,13 @@
+import { DataItem } from "@/types/type"
+import Card from "./Card"
+
+export default function Homes({ file }: { file: DataItem[] }) {
+  console.log(typeof file)
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 max-w-7xl mx-auto">
+      {file.map((f) => (
+        <Card key={f.id} name={f.name} description={`${f.price} for ${f.minimum_nights} nights`} imageUrl={f.picture_url}/>
+      ))}
+    </div>
+  )
+}
