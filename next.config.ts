1a5535110fa1b50001d7670193b88e224c908019
ferhadovss <ferhad.ov.ilqar0@gmail.com>
2026-01-09 19:11:21 +0400

    Minimal changes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    domains: ["a0.muscache.com"],
+  },
 };
 
 export default nextConfig;
